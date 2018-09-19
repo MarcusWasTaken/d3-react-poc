c4efe69042f6b563558b3a523f9b4505d818b39f
Marcus Göhle <the.macon7@gmail.com>
2018-09-19 12:38:51 +0200

    Added dummy data and reducers for gaps, concepts and relations

--- src/reducers.js
+++ src/reducers.js
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux'
+import data from 'containers/app/reducers'
 
-export default combineReducers({})
+export default combineReducers({
+  data
+})
--- src/store.js
+++ src/store.js
@@ -1,14 +1,19 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
+import data from './data'
 
 function store() {
   const composeEnhancers =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
   const middlewares = [thunk]
+  const preloadedState = {
+    data
+  }
 
   const store = createStore(
     rootReducer,
+    preloadedState,
     composeEnhancers(applyMiddleware(...middlewares))
   )
 
