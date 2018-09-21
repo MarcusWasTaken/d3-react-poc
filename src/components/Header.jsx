239d391a22e98d0d8d00a9e0618dd9cdcbea7b2e
Marcus Göhle <the.macon7@gmail.com>
2018-09-22 00:51:43 +0200

    Header now looks a bit better in mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,12 +7,15 @@ const Header = styled.header`
   padding-left: 56px;
   align-items: center;
   background: #0086bf;
-
   box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.2), 0 1px 10px 0 rgba(0, 0, 0, 0.14),
     0 2px 4px -1px rgba(0, 0, 0, 0.12);
 
   @media print {
     display: none;
+  }
+
+  @media (max-width: 640px) {
+    padding-left: 24px;
   }
 `
 
